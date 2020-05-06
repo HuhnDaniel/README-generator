b03abd2f93074516f22213bcbc2d11f6aa7a2aa5
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2020-05-06 15:41:38 -0500

    started writing generated md

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,14 @@
 function generateMarkdown(data) {
-    return `
-    # ${data.gitHubUser}
+    const repo = data.title.replace(" ", "-").toLowerCase();
+    console.log(repo);
 
-    `;
+    return `
+# ${data.title}
+![GitHub repo size](https://img.shields.io/github/repo-size/${data.gitHubUser}/${repo})
+## Description
+${data.description}
+`;
+
 }
 
 module.exports = generateMarkdown;
